99e74b615379c493402ae94f8d9a81bc3bf9adc3
Minh Luc <63479851+thatmeanlook@users.noreply.github.com>
2024-02-20 17:59:46 -0800

    island rotation WIP

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -12,10 +12,96 @@ import { useFrame, useThree } from "@react-three/fiber";
 import islandScene from '../assets/3d/island.glb'
 import { a } from '@react-spring/three'
 
-const Island = (props) => {
+const Island = ({ isRotating, setIsRotating, ...props }) => {
     const islandRef = useRef();
 
+    const { gl, viewport } = useThree();
     const { nodes, materials } = useGLTF(islandScene);
+
+    const lastX = useRef(0);
+    const rotationSpeed = useRef(0);
+    const dampingFactor = 0.95;
+
+    const handlePointerDown = (e) => {
+        e.stopProgation();
+        e.preventDefault();
+        setIsRotating(false);
+
+        const clientX = e.touches
+            ? e.touches[0].clientX
+            : e.clientX;
+
+        lastX.current = clientX;
+    }
+
+    const handlePointerUp = (e) => {
+        e.stopProgation();
+        e.preventDefault();
+        setIsRotating(true);
+
+        const clientX = e.touches
+            ? e.touches[0].clientX
+            : e.clientX;
+
+        const delta = (clientX - lastX.current) / viewport.width;
+
+        islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+        lastX.current = clientX;
+        rotationSpeed.current = delta * 0.01 * Math.PI;
+    }
+
+    const handlePointerMove = (e) => {
+        e.stopProgation();
+        e.preventDefault();
+
+        if (isRotating) handlePointerUp(e);
+
+    }
+
+    useEffect(() => {
+        document.addEventListener('pointerdown', handlePointerDown);
+        document.addEventListener('pointerup', handlePointerUp);
+        document.addEventListener('pointermove', handlePointerMove);
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+
+
+        return () => {
+            document.removeEventListener('pointerdown', handlePointerDown);
+            document.removeEventListener('pointerup', handlePointerUp);
+            document.removeEventListener('pointermove', handlePointerMove);
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowLeft') {
+            if (!isRotating) setIsRotating(true);
+            islandRef.current.rotation.y += 0.01 * Math.PI;
+        } else if (e.key === 'ArrowRight') {
+            if (!isRotating) setIsRotating(true);
+            islandRef.current.rotation.y -= 0.01 * Math.PI;
+        }
+    }
+
+    useFrame(() => {
+        if (!isRotating) {
+            rotationSpeed.current *= dampingFactor;
+
+            if (Math.abs(rotationSpeed.current) < 0.001) {
+                rotationSpeed.current = 0;
+            }
+        } else {
+            const rotation = islandRef.current.rotation.y;
+        }
+    })
+
+    const handleKeyUp = (e) => {
+        if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            setIsRotating(false);
+        }
+    }
 
     return (
         <a.group ref={islandRef} {...props}>
--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import planeScene from '../assets/3d/plane.glb'
 import { useGLTF } from '@react-three/drei'
 
-const Plane = () => {
+const Plane = ({ isRotating, ...props }) => {
 
     const { scene, animation } = useGLTF(planeScene);
 
     return (
-        <mesh>
+        <mesh {...props}>
             <primitive object={scene} />
         </mesh>
     )
