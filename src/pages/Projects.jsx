4329789daf4971e3dfb377ff5cc5fd9257fc4971
Minh Luc <63479851+thatmeanlook@users.noreply.github.com>
2024-03-01 16:42:15 -0800

    Projects page WIP

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { projects } from '../constants/index'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
+import CTA from '../components/CTA'
+
 
 const Projects = () => {
     return (
@@ -17,6 +22,55 @@ const Projects = () => {
                     What I have worked on
                 </p>
             </div>
+
+
+            <div className='flex flex-wrap my-20 gap-16'>
+                {projects.map((project) => (
+                    <div className='lg:w-[400px] w-full' key={project.name}>
+                        <div className='block-container w-12 h-12'>
+                            <div className={`btn-back rounded-xl ${project.theme}`} />
+                            <div className='btn-front rounded-xl flex justify-center items-center'>
+                                <img src={project.iconUrl}
+                                    atl="Project Icon"
+                                    className='w-1/2 h-1/2 object-contain' />
+                            </div>
+                        </div>
+
+                        <div className='mt-5 flex flex-col'>
+                            <h4 className='text-2xl font-poppins font-semibold'>
+                                {project.name}
+                            </h4>
+                            <p className='mt-2 text-slate-500'>
+                                {project.description}
+                            </p>
+
+                            <div className='mt-5 flex items-center gap-2 font-poppins'>
+                                <Link
+                                    to={project.link}
+                                    target="_blank"
+                                    rel='noopener noreferrer'
+                                    className='font-semibold text-blue-500'
+                                >
+                                    Live Link
+                                </Link>
+
+                                <img
+                                    src={arrow}
+                                    alt="Arrow"
+                                    className='w-4 h-4 object-contain' />
+
+                            </div>
+                        </div>
+
+                    </div>
+                ))}
+
+            </div>
+
+            <hr className='border-slate-200' />
+
+            <CTA />
+
         </section>
     )
 }
